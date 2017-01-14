51ede23afab5a76cb8a31b21402f9d16884ec1e5
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2017-01-14 18:42:55 -0300

    Consumiendo api para mostrar datos en el home

--- proyecto-comentarios/source/api.js
+++ proyecto-comentarios/source/api.js
@@ -1,28 +1,28 @@
 import fetch from 'isomorphic-fetch';
 
-const baseUrl = 'https://jsonplaceholder.typicode.com';
+const baseUrl = 'http://jsonplaceholder.typicode.com';
 
 const api = {
     posts: {
         async getList(page = 1){
-            const reponse = await fetch('${baseUrl}/posts?_page=${page}');
+            const response = await fetch(`${baseUrl}/posts?_page=${page}`);
             const data = await response.json();
             return data;
         },
         async getSingle(id = 1){
-            const response = await fetch('${baseUrl}/posts/${id}');
+            const response = await fetch(`${baseUrl}/posts/${id}`);
             const data = await response.json();
             return data;
         },
-        async getComment(id = 1){
-            const reponse = await fetch('${baseUrl}/posts/${id}/comments');
+        async getComments(id = 1){
+            const response = await fetch(`${baseUrl}/posts/${id}/comments`);
             const data = await response.json();
             return data;
         },
     },
     users: {
         async getSingle(id = 1){
-            const response = await fetch('${baseUrl}/users/${id}');
+            const response = await fetch(`${baseUrl}/users/${id}`);
             const data = await response.json();
             return data;
         },
--- proyecto-comentarios/source/pages/containers/Home.jsx
+++ proyecto-comentarios/source/pages/containers/Home.jsx
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
 
+import Post from '../../posts/containers/Post.jsx';
+import api from '../../api.js';
+
 class Home extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            page: 1,
+            posts: [],
+            loading: true,
+        };
+    }
+    async componentDidMount() {
+        const posts = await api.posts.getList(this.state.page);
+        this.setState({
+            posts,
+            page: this.state.page + 1,
+            loading: false,
+        }) 
+    }
     render() {
         return (
             <section name="Home">
                 <h1>Home</h1>
+
+                <section>
+                    {this.state.loading && (
+                        <h2>Loading posts...</h2>
+                    )}
+                    {this.state.posts
+                        .map(post => <Post key={post.id} {...post} />)
+                    }
+                </section>
                 <Link to="/aboutd">
                     Go to about
                 </Link>
