7cf5e9c2803a8daa28dcfa567eb781f4f49cf88b
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2017-01-16 23:44:52 -0300

    Paginacion de post mediante scroll

--- proyecto-comentarios/source/pages/containers/Home.jsx
+++ proyecto-comentarios/source/pages/containers/Home.jsx
@@ -15,7 +15,10 @@ class Home extends Component {
             posts: [],
             loading: true,
         };
+
+        this.handleScroll = this.handleScroll.bind(this);
     }
+
     async componentDidMount() {
         const posts = await api.posts.getList(this.state.page);
         this.setState({
@@ -23,19 +26,54 @@ class Home extends Component {
             page: this.state.page + 1,
             loading: false,
         }) 
+
+        window.addEventListener('scroll', this.handleScroll);
     }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll(event) {
+        if(this.state.loading) return null;
+
+        const scrolled = window.scrollY;
+        const viewportHeight = window.innerHeight;
+        const fullHeight = document.body.clientHeight;
+
+        if(!(scrolled + viewportHeight + 300 >= fullHeight)){
+            return null;
+        }
+
+        this.setState({ loading: true}, async () => {
+            try {
+                const posts = await api.posts.getList(this.state.page);
+
+                this.setState({
+                    posts: this.state.posts.concat(posts),
+                    page: this.state.page + 1,
+                    loading: false,
+                });
+            } catch(error) {
+                console.log(error);
+                this.setState({ loading: false});
+            }
+        })
+
+    }
+
     render() {
         return (
             <section name="Home">
                 <h1>Home</h1>
 
                 <section>
-                    {this.state.loading && (
-                        <Loading />
-                    )}
                     {this.state.posts
                         .map(post => <Post key={post.id} {...post} />)
                     }
+                    {this.state.loading && (
+                        <Loading />
+                    )}
                 </section>
             </section>
         );
