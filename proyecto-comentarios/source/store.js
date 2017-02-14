a49cae49e018fd724355f47662078626f265e0d7
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2017-02-14 02:02:27 -0300

    Implemtando un logger como middleware

--- proyecto-comentarios/source/store.js
+++ proyecto-comentarios/source/store.js
@@ -1,7 +1,22 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './reducer';
 
-const store = createStore(reducer);
+const logger = store => next => (action) => {
+  console.group('logger');
+  console.debug('estado actual', store.getState());
+  console.debug('accion', action);
+  const result = next(action);
+  console.debug('estado nuevo', store.getState());
+  console.groupEnd('logger');
+  return result;
+};
+
+const store = createStore(
+    reducer,
+    applyMiddleware(
+        logger,
+    ),
+);
 
 export default store;
