761f4824433c7e9544dbcfd5235dd13b8a9b0dc1
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2017-02-14 02:06:23 -0300

    Utilizando middleware de redux-logger

--- proyecto-comentarios/source/store.js
+++ proyecto-comentarios/source/store.js
@@ -1,21 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
 
 import reducer from './reducer';
-
-const logger = store => next => (action) => {
-  console.group('logger');
-  console.debug('estado actual', store.getState());
-  console.debug('accion', action);
-  const result = next(action);
-  console.debug('estado nuevo', store.getState());
-  console.groupEnd('logger');
-  return result;
-};
 
 const store = createStore(
     reducer,
     applyMiddleware(
-        logger,
+        createLogger(),
     ),
 );
 
